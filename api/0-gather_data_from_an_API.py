66451c197d9d2922423c965685d3fe27eedc2bd4
GabiAcosta <acostagabriel1311@gmail.com>
2023-08-21 10:16:35 -0700

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,26 +8,28 @@ import requests
 import json
 from sys import argv
 
+if __name__ == "__main__":
+    
+    if len(argv) != 2:
+        exit()
+        
+    user_req = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    user = json.loads(user_req.text)
+    user_todo_req = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    user_todo = json.loads(user_todo_req.text)
 
-if len(argv) != 2:
-    exit()
-user_req = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-user = json.loads(user_req.text)
-user_todo_req = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
-user_todo = json.loads(user_todo_req.text)
+    completed = 0
+    not_completed = 0
+    for task in user_todo:
+        if task["completed"]:
+            completed += 1
+        else:
+            not_completed += 1
 
-completed = 0
-not_completed = 0
-for task in user_todo:
-    if task["completed"]:
-        completed += 1
-    else:
-        not_completed += 1
-
-print(f"Employee {user['name']} is done with tasks "
-      f"({completed}/{not_completed + completed}):")
-for task in user_todo:
-    if task["completed"]:
-        print(f'\t {task["title"]}')
+    print(f"Employee {user['name']} is done with tasks "
+        f"({completed}/{not_completed + completed}):")
+    for task in user_todo:
+        if task["completed"]:
+            print(f'\t {task["title"]}')
