0faf2b0651f433971fa93db4dd3d1a9004bb6fd9
GabiAcosta <acostagabriel1311@gmail.com>
2023-08-21 08:07:16 -0700

    task 0 and README

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Employee Task API
+
+This script retrieves information about a user's completed and incomplete tasks
+using the jsonplaceholder.typicode.com API.
+"""
+import requests
+import json
+from sys import argv
+
+user_req = requests.get(
+    'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+user = json.loads(user_req.text)
+user_todo_req = requests.get(
+    'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+user_todo = json.loads(user_todo_req.text)
+
+completed = 0
+not_completed = 0
+for task in user_todo:
+    if task["completed"]:
+        completed += 1
+    else:
+        not_completed += 1
+
+print(f"Employee {user['name']} is done with tasks({completed}/{
+    not_completed + completed}): ")
+for task in user_todo:
+    if task["completed"]:
+        print(f'\t {task["title"]}')
