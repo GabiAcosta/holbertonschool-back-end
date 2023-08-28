876f1eb6ad6656a15fe9c258364c60904329bb7a
GabiAcosta <85908880+GabiAcosta@users.noreply.github.com>
2023-08-28 10:05:42 -0300

    Delete python_variable_annotations directory

--- python_variable_annotations/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Add Function
-
-This module defines a function that adds two floating-point numbers.
-"""
-def add(a: float, b:float) -> float:
-    """Adds two floating-point numbers."""
-    return a + b
